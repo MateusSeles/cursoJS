9158b460f61646defe8a44dc2786c9c34f4c72d4
Mateus Seles <114114748+MateusSeles@users.noreply.github.com>
2023-02-10 17:35:57 -0300

    Att

--- ex008.js
+++ ex008.js
@@ -1,7 +1,7 @@
 console.log('O console funcionou corretamente')
 console.log('--------------------------------------------')
 
-var vel = 70
+var vel = 80
 console.log(`A velocidade do seu carro é ${vel}km/h.`)
 if (vel > 60) {  // Condição Simples (um senão / uma opção)
     console.log('Você ultrapassou a velocidade permitida. MULTADO!!!')
