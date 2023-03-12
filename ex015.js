c53173b23b9942f6b74ef3e43471932a2c40a764
Mateus Seles <114114748+MateusSeles@users.noreply.github.com>
2023-03-12 18:00:36 -0300

    Java script

---
+++ ex015.js
@@ -0,0 +1,24 @@
+console.log('Vai começar!!')
+for(var c = 1; c <=4; c++) {
+    console.log(c)
+}
+console.log('FIM!!')
+
+
+
+
+
+
+
+
+
+/*var c = 1
+while (c <= 5) {
+    console.log(c)
+    c++
+}
+
+
+for(var c = 1;c <=2;c++) {
+    console.log(c)
+} */
